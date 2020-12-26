5ca1913b46e374b6949989769a3bad5607dc57d5
mypjb <280417314@qq.com>
2020-12-26 11:03:25 +0800

    [Config] Move config file to directory

--- docker.sh
+++ docker.sh
@@ -7,36 +7,59 @@ MYSQL_TAG="mysql:8.0.21"
 REDIS_TAG="redis:6.0.8"
 MONGO_TAG="mongo:6.0.8"
 NGINX_TAG="nginx:1.19.2"
+DNS_TAG="--cap-add=NET_ADMIN andyshinn/dnsmasq:2.81"
 NETWORK="cluster"
-COMMON_ARGS=" --restart=always --net=${NETWORK}"
+DOCKER_VOLUME_PATH=/var/lib/docker/volumes
+COMMON_ARGS=" run -d --restart=always --net=${NETWORK} -v /etc/resolv.conf:/etc/resolv.conf "
 #docker network create -d bridge $NETWORK
 
-#mysql
+##mysql
 
 #docker volume create mysql-conf && docker volume create mysql-data
-#docker run -d --name mysql $COMMON_ARGS -v mysql-conf:/etc/mysql -v mysql-data:/var/lib/mysql -v /etc/resolv.conf:/etc/resolv.conf -e MYSQL_ROOT_PASSWORD=${USER} -p 3306:3306 $MYSQL_TAG
+#docker $COMMON_ARGS --name mysql -v mysql-conf:/etc/mysql -v mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${USER} -p 3306:3306 $MYSQL_TAG
+#cp -r -f mysql/* ${DOCKER_VOLUME_PATH}/mysql-conf/_data/
+#docker restart mysql
 
-
-#redis
+##redis
 
 #docker volume create redis
-#echo -e 'bind 0.0.0.0\nloglevel verbose\ndatabases 16\nrequirepass ${PASSWORD}\nmaxmemory 256mb' > /var/lib/docker/volumes/redis/_data/redis.conf 
-#docker run -d --name redis $COMMON_ARGS -v redis:/usr/local/etc/redis -v /etc/resolv.conf:/etc/resolv.conf  -p 6379:6379 $REDIS_TAG redis-server /usr/local/etc/redis/redis.conf
+#docker $COMMON_ARGS --name redis -v redis:/usr/local/etc/redis -p 6379:6379 $REDIS_TAG redis-server /usr/local/etc/redis/redis.conf
+#cp -r -f redis/* ${DOCKER_VOLUME_PATH}/redis/_data/
+#docker restart redis
 
-#mongo
+##mongo
 
 #docker volume create mongo-conf && docker volume create mongo-data
-#echo -e 'dbpath=/data/db\nauth=true\nbind_ip=0.0.0.0\n' >  /var/lib/docker/volumes/mongo-conf/_data/mongod.conf 
-#docker run -d --name mongo $COMMON_ARGS -v mongo-conf:/etc/mongo -v mongo-data:/data/db -v /etc/resolv.conf:/etc/resolv.conf -p 27017:27017 -p 8081:8081 -e MONGO_INITDB_ROOT_USERNAME=${USER} -e MONGO_INITDB_ROOT_PASSWORD=${PASSWORD} $MONGO_TAG --config /etc/mongo/mongod.conf
+#docker $COMMON_ARGS --name mongo -v mongo-conf:/etc/mongo -v mongo-data:/data/db -p 27017:27017 -p 8081:8081 -e MONGO_INITDB_ROOT_USERNAME=${USER} -e MONGO_INITDB_ROOT_PASSWORD=${PASSWORD} $MONGO_TAG --config /etc/mongo/mongod.conf
+#cp -r -f mongo/* ${DOCKER_VOLUME_PATH}/mongo-conf/_data/
+#docker restart mongo
 
+##jenkins
 
 #docker volume create jenkins 
 #docker volume create nuget
 #docker volume create release
 #docker volume create yarn
-#docker run $COMMON_ARGS --name=jenkins -u=${USER} -p 8080:8080 -p 50000:50000 -v jenkins:/var/jenkins_home -v release:/root/release  -v $(which docker):/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -d ${JENKINS_TAG}
+#docker $COMMON_ARGS --name=jenkins -u=${USER} -p 8080:8080 -p 50000:50000 -v jenkins:/var/jenkins_home -v release:/root/release  -v $(which docker):/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -d ${JENKINS_TAG}
 
+##nginx
 #docker volume create nginx
 
-#docker run --name nginx -d -v nginx:/etc/nginx $COMMON_ARGS -p 80:80 -p 443:443 $NGINX_TAG
+#docker $COMMON_ARGS --name nginx -v nginx:/etc/nginx $COMMON_ARGS -p 80:80 -p 443:443 $NGINX_TAG
+#cp -r -f nginx/* ${DOCKER_VOLUME_PATH}/nginx/_data/
+#docker restart nginx
 
+##nuget
+#docker volume create baget
+#docker $COMMON_ARGS --name nuget-server -p 8082:80 --env-file baget/baget.env -v baget:/var/baget loicsharma/baget:latest
+
+##dnsmasq
+
+#docker volume create dnsmasq
+#docker $COMMON_ARGS --name=dns -p 53:53/tcp -p 53:53/udp -v dnsmasq:/etc $DNS_TAG
+#cp -f -r dnsmasq/* ${DOCKER_VOLUME_PATH}/dnsmasq/_data/
+#docker restart dns
+
+##build
+#docker pull mypjb/dotnet-core-sdk:5.0
+#docker pull andrewmackrodt/nodejs:12

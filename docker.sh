d9838a77d31d0c3df55a9296ab84a8657a541c33
mypjb <280417314@qq.com>
2020-12-28 21:53:44 +0800

    [Shell] remove resolve.conf mnt

--- docker.sh
+++ docker.sh
@@ -12,55 +12,55 @@ BAGET_TAG="loicsharma/baget:latest"
 BUILD_TAGS=(mypjb/dotnet-core-sdk:5.0 mypjb/dotnet-core-aspnet:5.0 andrewmackrodt/nodejs:12)
 NETWORK="cluster"
 DOCKER_VOLUME_PATH=/var/lib/docker/volumes
-COMMON_ARGS=" run -d --restart=always --net=${NETWORK} -v /etc/resolv.conf:/etc/resolv.conf "
-docker network create -d bridge $NETWORK
+COMMON_ARGS=" run -d --restart=always --net=${NETWORK} "
+#docker network create -d bridge $NETWORK
 
 ##mysql
 
-docker volume create mysql-conf && docker volume create mysql-data
+#docker volume create mysql-conf && docker volume create mysql-data
 docker $COMMON_ARGS --name mysql -v mysql-conf:/etc/mysql -v mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${PASSWORD} -p 3306:3306 $MYSQL_TAG
-cp -r -f mysql/* ${DOCKER_VOLUME_PATH}/mysql-conf/_data/
-docker restart mysql
+#cp -r -f mysql/* ${DOCKER_VOLUME_PATH}/mysql-conf/_data/
+#docker restart mysql
 
 ##redis
 
-docker volume create redis
+#docker volume create redis
 docker $COMMON_ARGS --name redis -v redis:/usr/local/etc/redis -p 6379:6379 $REDIS_TAG redis-server /usr/local/etc/redis/redis.conf
-cp -r -f redis/* ${DOCKER_VOLUME_PATH}/redis/_data/
-docker restart redis
+#cp -r -f redis/* ${DOCKER_VOLUME_PATH}/redis/_data/
+#docker restart redis
 
 ##mongo
 
-docker volume create mongo-conf && docker volume create mongo-data
+#docker volume create mongo-conf && docker volume create mongo-data
 docker $COMMON_ARGS --name mongo -v mongo-conf:/etc/mongo -v mongo-data:/data/db -p 27017:27017 -p 8081:8081 -e MONGO_INITDB_ROOT_USERNAME=${USER} -e MONGO_INITDB_ROOT_PASSWORD=${PASSWORD} $MONGO_TAG --config /etc/mongo/mongod.conf
-cp -r -f mongo/* ${DOCKER_VOLUME_PATH}/mongo-conf/_data/
-docker restart mongo
+#cp -r -f mongo/* ${DOCKER_VOLUME_PATH}/mongo-conf/_data/
+#docker restart mongo
 
 ##jenkins
 
-docker volume create jenkins 
-docker volume create nuget
-docker volume create release
-docker volume create yarn
+#docker volume create jenkins 
+#docker volume create nuget
+#docker volume create release
+#docker volume create yarn
 docker $COMMON_ARGS --name=jenkins -u=${USER} -p 8080:8080 -p 50000:50000 -v jenkins:/var/jenkins_home -v release:/root/release  -v $(which docker):/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -d ${JENKINS_TAG}
 
 ##nginx
-docker volume create nginx
+#docker volume create nginx
 
 docker $COMMON_ARGS --name nginx -v nginx:/etc/nginx -p 80:80 -p 443:443 $NGINX_TAG
-cp -r -f nginx/* ${DOCKER_VOLUME_PATH}/nginx/_data/
-docker restart nginx
+#cp -r -f nginx/* ${DOCKER_VOLUME_PATH}/nginx/_data/
+#docker restart nginx
 
 ##nuget
-docker volume create baget
+#docker volume create baget
 docker $COMMON_ARGS --name nuget-server -p 8082:80 --env-file baget/baget.env -v baget:/var/baget $BAGET_TAG 
 
 ##dnsmasq
 
-docker volume create dnsmasq
+#docker volume create dnsmasq
 docker $COMMON_ARGS --name=dns -p 53:53/tcp -p 53:53/udp -v dnsmasq:/etc $DNS_TAG
-cp -f -r dnsmasq/* ${DOCKER_VOLUME_PATH}/dnsmasq/_data/
-docker restart dns
+#cp -f -r dnsmasq/* ${DOCKER_VOLUME_PATH}/dnsmasq/_data/
+#docker restart dns
 
 ##build
 

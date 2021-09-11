efc41704ff6be61e5727433e5d60f0e8c65a3c1d
pjb <280417314@qq.com>
2021-09-11 11:31:36 +0800

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,47 +1,59 @@
 #!/bin/bash
 
-echo install apt-key
+apt-get install -y libseccomp2
 
-apt install -y gnupg curl
+curl -L -o cri-containerd-cni-linux-amd64.tar.gz https://github.com/containerd/containerd/releases/download/v1.5.5/cri-containerd-cni-1.5.5-linux-amd64.tar.gz
 
-apt-key add k8s/apt-key.gpg
+tar --no-overwrite-dir -C / -xzf cri-containerd-cni-linux-amd64.tar.gz
 
-echo download kubelet kubeadm kubectl kubernetes-cni cri-tools package
+rm -rf cri-containerd-cni-linux-amd64.tar.gz
 
-curl -L -o kubelet.deb https://mirrors.tuna.tsinghua.edu.cn/kubernetes/apt/pool/kubelet_1.22.1-00_amd64_240cc59b5f8e44719af21b90161d32297679376f5a4d45ffd4795685ef305538.deb
-curl -L -o kubeadm.deb https://mirrors.tuna.tsinghua.edu.cn/kubernetes/apt/pool/kubeadm_1.22.1-00_amd64_2e8580f3165ea2f19ba84ac4237dad873cc9915214b3f8f5ca2e98c7a8ecd3e1.deb
-curl -L -o kubectl.deb https://mirrors.tuna.tsinghua.edu.cn/kubernetes/apt/pool/kubectl_1.22.1-00_amd64_2a00cd912bfa610fe4932bc0a557b2dd7b95b2c8bff9d001dc6b3d34323edf7d.deb
+systemctl enable containerd
 
-curl -L -o kubernetes-cni.deb https://mirrors.tuna.tsinghua.edu.cn/kubernetes/apt/pool/kubernetes-cni_0.8.7-00_amd64_ca2303ea0eecadf379c65bad855f9ad7c95c16502c0e7b3d50edcb53403c500f.deb
+mkdir -p /etc/systemd/system/kubelet.service.d
 
-curl -L -o cri-tools.deb https://mirrors.tuna.tsinghua.edu.cn/kubernetes/apt/pool/cri-tools_1.13.0-01_amd64_4ff4588f5589826775f4a3bebd95aec5b9fb591ba8fb89a62845ffa8efe8cf22.deb
-echo installation dependency
+cat <<EOF | tee /etc/systemd/system/kubelet.service.d/0-containerd.conf
+[Service]
+Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///run/containerd/containerd.sock --cgroup-driver=systemd"
+EOF
 
-apt install -y iptables socat ebtables ethtool conntrack
+cat <<EOF | tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
 
-echo install cri-tools
-dpkg -i cri-tools.deb
+modprobe overlay
+modprobe br_netfilter
 
-echo install kubernetes-cni
-dpkg -i kubernetes-cni.deb
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
 
-echo install kubelet
-dpkg -i kubelet.deb 
 
-echo kubectl kubelet
-dpkg -i kubectl.deb 
+cat <<EOF |  tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
 
-echo install kubeadm
-dpkg -i kubeadm.deb 
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
 
-kubeadm config images pull --image-repository  registry.cn-hangzhou.aliyuncs.com/mypjb
+sysctl --system
 
-ctr -n k8s.io images tag registry.cn-hangzhou.aliyuncs.com/mypjb/kube-apiserver:v1.22.1 k8s.gcr.io/kube-apiserver:v1.22.1
-ctr -n k8s.io images tag registry.cn-hangzhou.aliyuncs.com/mypjb/kube-controller-manager:v1.22.1 k8s.gcr.io/kube-controller-manager:v1.22.1
-ctr -n k8s.io images tag registry.cn-hangzhou.aliyuncs.com/mypjb/kube-scheduler:v1.22.1 k8s.gcr.io/kube-scheduler:v1.22.1
-ctr -n k8s.io images tag registry.cn-hangzhou.aliyuncs.com/mypjb/kube-proxy:v1.22.1 k8s.gcr.io/kube-proxy:v1.22.1
-ctr -n k8s.io images tag registry.cn-hangzhou.aliyuncs.com/mypjb/pause:3.5 k8s.gcr.io/pause:3.5
-ctr -n k8s.io images tag registry.cn-hangzhou.aliyuncs.com/mypjb/etcd:3.5.0-0 k8s.gcr.io/etcd:3.5.0-0
-ctr -n k8s.io images tag registry.cn-hangzhou.aliyuncs.com/mypjb/coredns:v1.8.4 k8s.gcr.io/coredns:v1.8.4
+mkdir -p /etc/containerd
 
-echo -e "\tNow initialize the cluster with the following command\n\tkubeadm init --image-repository  registry.cn-hangzhou.aliyuncs.com/mypjb --node-name $(hostname) --control-plane-endpoint $(hostname)  --v=5"
+containerd config default | tee /etc/containerd/config.toml
+
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+
+systemctl daemon-reload
+
+systemctl start containerd
+
+crictl config --set runtime-endpoint=unix:///run/containerd/containerd.sock 
+crictl config --set image-endpoint=unix:///run/containerd/containerd.sock
+
+echo -e '\tIf you do not configure the default kubedm container runtime, the following commands are recommended \n\techo -e "export CONTAINER_RUNTIME_ENDPOINT=unix:///run/containerd/containerd.sock\\nexport IMAGE_SERVICE_ENDPOINT=unix:///run/containerd/containerd.sock" >> /etc/profile'

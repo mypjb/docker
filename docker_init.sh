706f97064d1a848b1baec9af52bc655d92e58c2a
小姐请留步 <280417314@qq.com>
2021-10-29 22:39:06 +0800

    [k8s] Modify images version

--- docker_init.sh
+++ docker_init.sh
@@ -3,12 +3,14 @@
 bash_dir=$(cd $(dirname $0); pwd)
 bash_dir=$bash_dir/docker
 
+k8s_version=v1.22.3
+
 cd $bash_dir
 
-echo FROM k8s.gcr.io/kube-apiserver:v1.22.1 > kube-apiserver
-echo FROM k8s.gcr.io/kube-controller-manager:v1.22.1 > kube-controller-manager
-echo FROM k8s.gcr.io/kube-scheduler:v1.22.1 > kube-scheduler
-echo FROM k8s.gcr.io/kube-proxy:v1.22.1 > kube-proxy
+echo FROM k8s.gcr.io/kube-apiserver:${k8s_version} > kube-apiserver
+echo FROM k8s.gcr.io/kube-controller-manager:${k8s_version} > kube-controller-manager
+echo FROM k8s.gcr.io/kube-scheduler:${k8s_version} > kube-scheduler
+echo FROM k8s.gcr.io/kube-proxy:${k8s_version} > kube-proxy
 echo FROM k8s.gcr.io/pause:3.5 > pause
 echo FROM k8s.gcr.io/etcd:3.5.0-0 > etcd
 echo FROM k8s.gcr.io/coredns/coredns:v1.8.4 > coredns

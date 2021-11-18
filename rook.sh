3d4fd5d4a7c4f11465b18c8e2e494830db7b7466
小姐请留步 <280417314@qq.com>
2021-11-18 20:15:40 +0800

    [k8s] Modify rook image ns

--- rook.sh
+++ rook.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-#set -o errexit
+set -o errexit
 
 image_url=registry.cn-beijing.aliyuncs.com/mypjb
-k8s_np=k8s.io
+ctr_ns=k8s.io
+k8s_ns=k8s.gcr.io
 
-image_commands[0]="ctr -n ${k8s_np} image pull ${image_url}/sig-storage-csi-snapshotter:v4.2.0 && ctr -n ${k8s_np} image tag  ${image_url}/sig-storage-csi-snapshotter:v4.2.0 ${k8s_np}/sig-storage/csi-snapshotter:v4.2.0"
+image_commands[0]="ctr -n ${ctr_ns} image pull ${image_url}/sig-storage-csi-snapshotter:v4.2.0 && ctr -n ${ctr_ns} image tag  ${image_url}/sig-storage-csi-snapshotter:v4.2.0 ${k8s_ns}/sig-storage/csi-snapshotter:v4.2.0"
 
-image_commands[1]="ctr -n ${k8s_np} image pull ${image_url}/sig-storage-csi-node-driver-registrar:v2.3.0 && ctr -n ${k8s_np} image tag  ${image_url}/sig-storage-csi-node-driver-registrar:v2.3.0 ${k8s_np}/sig-storage/csi-node-driver-registrar:v2.3.0"
+image_commands[1]="ctr -n ${ctr_ns} image pull ${image_url}/sig-storage-csi-node-driver-registrar:v2.3.0 && ctr -n ${ctr_ns} image tag  ${image_url}/sig-storage-csi-node-driver-registrar:v2.3.0 ${k8s_ns}/sig-storage/csi-node-driver-registrar:v2.3.0"
 
-image_commands[2]="ctr -n ${k8s_np} image pull ${image_url}/sig-storage-csi-resizer:v1.3.0 && ctr -n ${k8s_np} image tag ${image_url}/sig-storage-csi-resizer:v1.3.0 ${k8s_np}/sig-storage/csi-resizer:v1.3.0"
+image_commands[2]="ctr -n ${ctr_ns} image pull ${image_url}/sig-storage-csi-resizer:v1.3.0 && ctr -n ${ctr_ns} image tag ${image_url}/sig-storage-csi-resizer:v1.3.0 ${k8s_ns}/sig-storage/csi-resizer:v1.3.0"
 
-image_commands[3]="ctr -n ${k8s_np} image pull ${image_url}/sig-storage-csi-provisioner:v3.0.0 && ctr -n ${k8s_np} image tag ${image_url}/sig-storage-csi-provisioner:v3.0.0 ${k8s_np}/sig-storage/csi-provisioner:v3.0.0"
+image_commands[3]="ctr -n ${ctr_ns} image pull ${image_url}/sig-storage-csi-provisioner:v3.0.0 && ctr -n ${ctr_ns} image tag ${image_url}/sig-storage-csi-provisioner:v3.0.0 ${k8s_ns}/sig-storage/csi-provisioner:v3.0.0"
 
-image_commands[4]="ctr -n ${k8s_np} image pull ${image_url}/sig-storage-csi-attacher:v3.3.0 && ctr -n ${k8s_np} image tag  ${image_url}/sig-storage-csi-attacher:v3.3.0 ${image_url}/sig-storage-csi-attacher:v3.3.0 ${k8s_np}/sig-storage/csi-attacher:v3.3.0"
+image_commands[4]="ctr -n ${ctr_ns} image pull ${image_url}/sig-storage-csi-attacher:v3.3.0 && ctr -n ${ctr_ns} image tag  ${image_url}/sig-storage-csi-attacher:v3.3.0 ${image_url}/sig-storage-csi-attacher:v3.3.0 ${k8s_ns}/sig-storage/csi-attacher:v3.3.0"
 
 if [ "${1}" == "pull" ];then
 
